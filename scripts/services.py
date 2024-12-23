76b48ef7802825712b7d1bfb0dcad4dc5d940e56
Krishna <sairamkrishna1412@gmail.com>
2024-12-23 17:50:28 +0530

    mypy done

--- scripts/services.py
+++ scripts/services.py
@@ -11,36 +11,36 @@ CLOTHES_COUNT_KEY = "CLOTHES_COUNT"
 SERVICE_ID_KEY = "SERVICE_ID"
 MAKE_NEW_ORDER = "MAKE_NEW_ORDER"
 
-BUTTONS: dict = {
-    f"{CLOTHES_COUNT_KEY}_15_TO_25": {
-        "id": f"{CLOTHES_COUNT_KEY}_15_TO_25",
-        "title": "15-25",
-    },
-    f"{CLOTHES_COUNT_KEY}_25_PLUS": {
-        "id": f"{CLOTHES_COUNT_KEY}_25_PLUS",
-        "title": "25+",
-    },
-    MAKE_NEW_ORDER: {"id": "MAKE_NEW_ORDER", "title": "Make New Order"},
-    "HOW_WE_WORK": {"id": "HOW_WE_WORK", "title": "How we work?"},
-    "TRACK_ORDER": {"id": "TRACK_ORDER", "title": "Track order"},
-    "REFER": {"id": "REFER", "title": "Refer"},
-    "PRICES": {"id": "PRICES", "title": "Prices"},
-    f"{SERVICE_ID_KEY}_1": {
-        "id": f"{SERVICE_ID_KEY}_1",
-        "title": "Iron",
-        "description": None,
-    },
-    f"{SERVICE_ID_KEY}_2": {
-        "id": f"{SERVICE_ID_KEY}_2",
-        "title": "Wash",
-        "description": None,
-    },
-    f"{SERVICE_ID_KEY}_3": {
-        "id": f"{SERVICE_ID_KEY}_3",
-        "title": "Wash & Iron",
-        "description": None,
-    },
-}
+# BUTTONS: dict = {
+#     f"{CLOTHES_COUNT_KEY}_15_TO_25": {
+#         "id": f"{CLOTHES_COUNT_KEY}_15_TO_25",
+#         "title": "15-25",
+#     },
+#     f"{CLOTHES_COUNT_KEY}_25_PLUS": {
+#         "id": f"{CLOTHES_COUNT_KEY}_25_PLUS",
+#         "title": "25+",
+#     },
+#     MAKE_NEW_ORDER: {"id": "MAKE_NEW_ORDER", "title": "Make New Order"},
+#     "HOW_WE_WORK": {"id": "HOW_WE_WORK", "title": "How we work?"},
+#     "TRACK_ORDER": {"id": "TRACK_ORDER", "title": "Track order"},
+#     "REFER": {"id": "REFER", "title": "Refer"},
+#     "PRICES": {"id": "PRICES", "title": "Prices"},
+#     f"{SERVICE_ID_KEY}_1": {
+#         "id": f"{SERVICE_ID_KEY}_1",
+#         "title": "Iron",
+#         "description": None,
+#     },
+#     f"{SERVICE_ID_KEY}_2": {
+#         "id": f"{SERVICE_ID_KEY}_2",
+#         "title": "Wash",
+#         "description": None,
+#     },
+#     f"{SERVICE_ID_KEY}_3": {
+#         "id": f"{SERVICE_ID_KEY}_3",
+#         "title": "Wash & Iron",
+#         "description": None,
+#     },
+# }
 
 
 async def add_service_locations():
