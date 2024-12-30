0084053ed7884a66a2ce39e0359c7cb3a5df3991
Krishna <sairamkrishna1412@gmail.com>
2024-12-30 18:45:20 +0530

    BE: Update login endpoint to support cookie-based token storage and extend access token expiration to 7 days

--- irony/routers/ironman.py
+++ irony/routers/ironman.py
@@ -1,5 +1,6 @@
-from datetime import timedelta
-from fastapi import APIRouter, HTTPException
+from datetime import datetime, timedelta, timezone
+import time
+from fastapi import APIRouter, HTTPException, Response
 
 
 from irony.db import db
@@ -17,37 +18,57 @@ router = APIRouter()
 # Below is placeholder code
 
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
+
 
 @router.post("/login")
-async def login(user: UserLogin):
+async def login(response: Response, user: UserLogin):
     db_user = await db.service_agent.find_one({"mobile": user.mobile})
     if not db_user:
         raise HTTPException(status_code=401, detail="Invalid mobile or password")
-    
+
     db_user = ServiceAgent(**db_user)
-    if auth.verify_password(user.password, db_user.password):
+    if not auth.verify_password(user.password, db_user.password):
         raise HTTPException(status_code=401, detail="Invalid mobile or password")
-    
+
     # Generate JWT token
     token = auth.create_access_token(
         data={"sub": user.mobile},
-        expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
     )
-    return {"access_token": token, "token_type": "bearer", "user": db_user.model_dump(exclude={"password"})}
+
+    response.set_cookie(
+        key="auth_token",
+        value=token,
+        httponly=True,
+        # secure=True,  # Use True in production with HTTPS
+        secure=False,  # Use True in production with HTTPS
+        samesite="None",
+        expires=datetime.now(timezone.utc)
+        + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        "user": db_user.model_dump(exclude={"password"}),
+    }
+
 
 @router.post("/register")
 async def register(user: ServiceAgentRegister):
     # Check if passwords match
     if not user.mobile or not user.password or not user.confirm_password:
-        raise HTTPException(status_code=400, detail="Mobile, Password or confirm password not provided")
-    
+        raise HTTPException(
+            status_code=400, detail="Mobile, Password or confirm password not provided"
+        )
+
     if not user.name:
         raise HTTPException(status_code=400, detail="Name not provided")
-    
+
     if not user.service_location_id:
         raise HTTPException(status_code=400, detail="Service location not provided")
-    
+
     if user.password != user.confirm_password:
         raise HTTPException(status_code=400, detail="Passwords do not match")
 
@@ -65,9 +86,12 @@ async def register(user: ServiceAgentRegister):
 
     service_agent.id = result.inserted_id
 
-    return {"message": "User registered successfully", "user": service_agent.model_dump(exclude={"password"})}
+    return {
+        "message": "User registered successfully",
+        "user": service_agent.model_dump(exclude={"password"}),
+    }
+
 
 @router.get("/protected-route")
 async def protected_route(current_user: str = Depends(auth.get_current_user)):
     return {"message": f"Welcome, {current_user}!"}
-
