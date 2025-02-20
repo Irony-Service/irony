b42c74e26a0e2dfd82d38d933aa9a52c6625ab2e
Krishna <sairamkrishna1412@gmail.com>
2025-02-21 01:09:18 +0530

    Refactor whatsapp router to improve logging and enhance message handling with asynchronous sleep

--- irony_backend/irony/routers/whatsapp.py
+++ irony_backend/irony/routers/whatsapp.py
@@ -1,13 +1,14 @@
+import asyncio
+import random
 import traceback
+
 from fastapi import APIRouter, Request, Response
 
-from irony.util import whatsapp_utils
-from ..config import config
 from irony.config.logger import logger
-
 from irony.services.whatsapp import whatsapp_service
+from irony.util import whatsapp_utils
 
-
+from ..config import config
 from ..models.user import User
 
 router = APIRouter()
@@ -26,12 +27,17 @@ async def whatsapp(request: Request):
                 messages = value.get("messages", [])
                 for message in messages:
                     try:
+                        random_number = random.randint(1, 1000)
+                        logger.info(f"Calls {random_number}: {config.CALLS}")
+                        logger.info(f"Sleeping for 50 ms: {random_number}")
+                        await asyncio.sleep(0.05)
+                        logger.info(f"Completed 50 ms: {random_number}")
+                        logger.info(f"Calls After sleep{random_number}: {config.CALLS}")
                         if whatsapp_service.is_ongoing_or_status_request(message):
                             return Response(status_code=200)
-                        else:
-                            return await whatsapp_service.handle_entry(
-                                message, contacts_details_dict[message["from"]]
-                            )
+                        return await whatsapp_service.handle_entry(
+                            message, contacts_details_dict[message["from"]]
+                        )
                     except Exception as e:
                         logger.error(f"Error occured in send whatsapp message : {e}")
                         traceback.print_exc()
